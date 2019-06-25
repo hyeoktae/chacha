2e7a3e93c9ba2007405d2bba405af1caff91fe4f
hyeoktae kwon <kht_0218@hanmail.net>
2019-06-26 01:17:36 +0900

    20190626 - 권혁태 - 재업로드

--- Chacha/Chacha/AppDelegate.swift
+++ Chacha/Chacha/AppDelegate.swift
@@ -23,10 +23,10 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
   private let mainVC = MainVC()
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    
-    window = UIWindow(frame: UIScreen.main.bounds)
     checkUUID()
     setupBeacon()
+    
+    window = UIWindow(frame: UIScreen.main.bounds)
     window?.rootViewController = mainVC
     window?.backgroundColor = .white
     window?.makeKeyAndVisible()
