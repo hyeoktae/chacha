edae46914f1cc38dd328ba17fccdb638df2434cc
chasuyeon <sy9600@naver.com>
2019-06-25 21:38:48 +0900

    20190625 - 차수연 - MainVC 작업중

--- Chacha/Chacha/Controller/MainVC.swift
+++ Chacha/Chacha/Controller/MainVC.swift
@@ -8,11 +8,44 @@
 
 import UIKit
 
-class MainVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+class MainVC: UIViewController {
+  
+  var uuid = UserDefaults.standard.string(forKey: "uuid")
+  
+  let registerVC = RegisterVC()
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    checkUUID()
+  }
+  
+  private func checkUUID() {
+    let baseUUID = UserDefaults.standard.string(forKey: "uuid")
+    
+    if baseUUID == nil { //학생등록X -> 이름 & 스쿨 & 우리집 등록(map) 화면
+      uuid = UUID.init().uuidString
+      UserDefaults.standard.set(uuid, forKey: "uuid")
+      print("최초생성한 UUID: ", uuid)
+      
+      present(RegisterVC(), animated: true)
+      
+    } else {
+      print("생성된 UUID", uuid)
     }
     
+  }
+  
+  
+  
+  
+  
 }
+
