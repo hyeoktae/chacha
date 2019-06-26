5ea9479693dff382311928eccf55e0f2936ca0db
hyeoktae kwon <kht_0218@hanmail.net>
2019-06-26 15:22:56 +0900

    feat: tass - merge suyeon & tass

--- Chacha/Chacha/Controller/MainVC.swift
+++ Chacha/Chacha/Controller/MainVC.swift
@@ -25,12 +25,6 @@ final class MainVC: UIViewController {
     firstRunApp()
   }
   
-  
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    firstRunApp()
-  }
-  
   // appdelegate 에서 넘어오는 uuid값의 유무 확인 후 nil이면 present
   private func firstRunApp() {
     guard myUUID == nil else { return }
