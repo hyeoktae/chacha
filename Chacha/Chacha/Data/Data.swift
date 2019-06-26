daf1ee5c0b08925177a807c114edd606251b84b5
hyeoktae kwon <kht_0218@hanmail.net>
2019-06-27 01:19:32 +0900

    metge: 전부삭제

--- Chacha/Chacha/Data/Data.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Data.swift
-//  Chacha
-//
-//  Created by hyeoktae kwon on 2019/06/25.
-//  Copyright © 2019 hyeoktae kwon. All rights reserved.
-//
-
-import Foundation
--- Chacha/Pods/Target Support Files/Pods-Chacha/Pods-Chacha-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Chacha : NSObject
-@end
-@implementation PodsDummy_Pods_Chacha
-@end
